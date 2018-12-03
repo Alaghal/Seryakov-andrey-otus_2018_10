a5ee74ac0a35d2af6c607bf9ae3b6da8693bd993
Andrey <alahal@mail.ru>
2018-12-03 23:08:15 +0700

    fixed MyArralist, add iterator of 11 version Array list

--- L03Collection/src/main/java/ru/otus/l2/memory/Main.java
+++ L03Collection/src/main/java/ru/otus/l2/memory/Main.java
@@ -1,11 +1,56 @@
 package ru.otus.l2.memory;
 
+import java.util.*;
+
 public class Main {
     public static void main(String... args){
+
+
+        List<Integer> ls = new ArrayList<Integer> ();
+        ls.add(3);
+        ls.add(5);
+        ls.add(6);
+        ls.add(8);
+        ls.add(0);
+
+
+
+
+       List <Integer> mls = new MyArrayList<Integer>();
+
+        boolean  b =  Collections.addAll(mls,1,2,3,4,5,8);
+        System.out.println(mls);
+
+
+        //Collections.copy(mls,ls);
+
+     //   MyArrayList.copy(mls,ls);
+     //  MyArrayList.sort(mls, new IntegerComparator());
+
+       // System.out.println(mls);
+
+      //  MyArrayList.cop
+
 
 
 
     }
 
 
+}
+
+class IntegerComparator implements Comparator<Integer> {
+
+
+    @Override
+    public int compare(Integer o1, Integer o2) {
+
+        if((int)o1 > (int)o2) {
+            return 1;
+        }
+        else if( (int)o1 < (int)o2)
+            return -1;
+        else
+            return 0;
+    }
 }
--- L03Collection/src/main/java/ru/otus/l2/memory/MyArrayList.java
+++ L03Collection/src/main/java/ru/otus/l2/memory/MyArrayList.java
@@ -1,14 +1,28 @@
 package ru.otus.l2.memory;
 
 import java.util.*;
+import java.util.function.Consumer;
 
-public class MyArrayList<E> implements List<E> {
+public class MyArrayList<E> extends ArrayList<E> implements List<E> {
 
     private int size;
 
     private static final int DEFAULT_CAPACITY = 10;
 
     private transient E[] data;
+
+    public MyArrayList()
+    {
+       this(DEFAULT_CAPACITY);
+    }
+
+    public MyArrayList(int capacity) {
+
+        if (capacity < 0)
+            throw new IllegalArgumentException();
+        data = (E[]) new Object[capacity];
+    }
+
 
     public int size() {
 
@@ -24,83 +38,230 @@ public class MyArrayList<E> implements List<E> {
         return indexOf(o) != -1;
     }
 
-    public Iterator iterator() {
-        return null;
+    static <E> E elementAt(Object[] es, int index) {
+        return (E) es[index];
     }
 
-    public Object[] toArray() {
-        return new Object[0];
+    public Iterator<E> iterator() {
+        return new MyArrayList.Itr();
     }
 
-    public boolean add(Object o) {
-        return false;
+    /**
+     * An optimized version of AbstractList.Itr
+     */
+    private class Itr implements Iterator<E> {
+        int cursor;       // index of next element to return
+        int lastRet = -1; // index of last element returned; -1 if no such
+        int expectedModCount = modCount;
+
+        // prevent creating a synthetic constructor
+        Itr() {}
+
+        public boolean hasNext() {
+            return cursor != size;
+        }
+
+        @SuppressWarnings("unchecked")
+        public E next() {
+            checkForComodification();
+            int i = cursor;
+            if (i >= size)
+                throw new NoSuchElementException();
+            Object[] elementData = MyArrayList.this.data;
+            if (i >= elementData.length)
+                throw new ConcurrentModificationException();
+            cursor = i + 1;
+            return (E) elementData[lastRet = i];
+        }
+
+        public void remove() {
+            if (lastRet < 0)
+                throw new IllegalStateException();
+            checkForComodification();
+
+            try {
+                MyArrayList.this.remove(lastRet);
+                cursor = lastRet;
+                lastRet = -1;
+                expectedModCount = modCount;
+            } catch (IndexOutOfBoundsException ex) {
+                throw new ConcurrentModificationException();
+            }
+        }
+
+        @Override
+        public void forEachRemaining(Consumer<? super E> action) {
+            Objects.requireNonNull(action);
+            final int size = MyArrayList.this.size;
+            int i = cursor;
+            if (i < size) {
+                final Object[] es = data;
+                if (i >= es.length)
+                    throw new ConcurrentModificationException();
+                for (; i < size && modCount == expectedModCount; i++)
+                    action.accept(elementAt(es, i));
+                // update once at end to reduce heap write traffic
+                cursor = i;
+                lastRet = i - 1;
+                checkForComodification();
+            }
+        }
+
+
+
+        final void checkForComodification() {
+            if (modCount != expectedModCount)
+                throw new ConcurrentModificationException();
+        }
     }
 
-    public boolean remove(Object o) {
-        return false;
-    }
+            public Object[] toArray() {
+                return new Object[0];
+            }
 
-    public boolean addAll(Collection c) {
-        return false;
-    }
 
-    public boolean addAll(int index, Collection c) {
-        return false;
-    }
+            public boolean add(E o) {
+                modCount++;
+                if (size == data.length)
+                    ensureCapacity(size + 1);
+                data[size++] = o;
+                return true;
+            }
 
-    public void clear() {
+            public void ensureCapacity(int minCapacity) {
 
-    }
+                int current = data.length;
 
-    public Object get(int index) {
-        return null;
-    }
+                if (minCapacity > current) {
 
-    public Object set(int index, Object element) {
-        return null;
-    }
+                    E[] newData = (E[]) new Object[Math.max(current * 2, minCapacity)];
+                    System.arraycopy(data, 0, newData, 0, size);
+                    data = newData;
+                }
 
-    public void add(int index, Object element) {
+            }
 
-    }
+            public boolean remove(Object o) {
+                return false;
+            }
 
-    public Object remove(int index) {
-        return null;
-    }
+            public boolean addAll(Collection<? extends E> c) {
+                return addAll(size, c);
+            }
 
-    public int indexOf(Object o) {
-        return 0;
-    }
+            public boolean addAll(int index, Collection c) {
 
-    public int lastIndexOf(Object o) {
-        return 0;
-    }
+                Iterator<? extends E> itr = c.iterator();
+                int csize = c.size();
 
-    public ListIterator listIterator() {
-        return null;
-    }
+                modCount++;
+                if (csize + size > data.length)
+                    ensureCapacity(size + csize);
+                int end = index + csize;
+                if (size > 0 && index != size)
+                    System.arraycopy(data, index, data, end, size - index);
+                size += csize;
+                for (; index < end; index++)
+                    data[index] = itr.next();
+                return csize > 0;
+            }
 
-    public ListIterator listIterator(int index) {
-        return null;
-    }
+            public void clear() {
 
-    public List subList(int fromIndex, int toIndex) {
-        return null;
-    }
+                if (size > 0) {
+                    modCount++;
+                    Arrays.fill(data, 0, size, null);
+                    size = 0;
+                }
 
-    public boolean retainAll(Collection c) {
-        return false;
-    }
+            }
 
-    public boolean removeAll(Collection c) {
-        return false;
-    }
 
-    public boolean containsAll(Collection c) {
-        return false;
-    }
+            public E get(int index) {
 
-    public Object[] toArray(Object[] a) {
-        return new Object[0];
-    }
-}
+                return data[index];
+            }
+
+            public E set(int index, E element) {
+
+                E result = data[index];
+                data[index] = element;
+                return result;
+            }
+
+            public void add(int index, E element) {
+                modCount++;
+                if (size == data.length)
+                    ensureCapacity(size + 1);
+                if (index != size)
+                    System.arraycopy(data, index, data, index + 1, size - index);
+                data[index] = element;
+                size++;
+
+            }
+
+            public E remove(int index) {
+                E r = data[index];
+                modCount++;
+                if (index != --size)
+                    System.arraycopy(data, index + 1, data, index, size - index);
+                data[size] = null;
+                return r;
+            }
+
+            public int indexOf(Object o) {
+                return 0;
+            }
+
+            public int lastIndexOf(Object o) {
+                return 0;
+            }
+
+            public ListIterator listIterator() {
+                return null;
+            }
+
+            public ListIterator listIterator(int index) {
+                return null;
+            }
+
+            public List subList(int fromIndex, int toIndex) {
+                return null;
+            }
+
+            public boolean retainAll(Collection c) {
+                return false;
+            }
+
+            public boolean removeAll(Collection c) {
+                return false;
+            }
+
+            public boolean containsAll(Collection c) {
+                return false;
+            }
+
+            public Object clone() {
+                MyArrayList clone = null;
+                try {
+                    clone = (MyArrayList<E>) super.clone();
+                    ((MyArrayList) clone).data = (E[]) data.clone();
+                } catch (Exception e) {
+
+                }
+                return clone;
+            }
+
+
+            public Object[] toArray(Object[] a) {
+                return new Object[0];
+            }
+
+            public void sort(Comparator<? super E> c) {
+                final int expectedModCount = modCount;
+                Arrays.sort((E[]) data, 0, size, c);
+                if (modCount != expectedModCount)
+                    throw new ConcurrentModificationException();
+                modCount++;
+            }
+        }
