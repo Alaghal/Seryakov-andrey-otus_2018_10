4528565b9d056649a1b3a99fc0d477c015f5cf4d
Andrey <alahal@mail.ru>
2019-01-17 19:52:29 +0700

    Configurate for 3 mark

--- L06-cashe/src/main/java/ru/otus/l6/cacheForHW/MyCache.java
+++ L06-cashe/src/main/java/ru/otus/l6/cacheForHW/MyCache.java
@@ -1,21 +1,16 @@
 package ru.otus.l6.cacheForHW;
 
-import java.lang.ref.Reference;
-import java.lang.ref.ReferenceQueue;
+
 import java.lang.ref.SoftReference;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * @author sergey
- * created on 14.12.18.
- */
+
 public class MyCache<K, V> implements HwCache<K, V> {
-//Надо реализовать эти методы
-    private final Map<K,SoftValue <V>> elementCache;
-    private ReferenceQueue<V> queue = new ReferenceQueue<V>();
+    
+    private final Map<SoftReference<K>,SoftReference<V>> elementCache;
     private final List<HwListener> listenersList;
 
     MyCache(){
@@ -23,23 +18,9 @@ public class MyCache<K, V> implements HwCache<K, V> {
         listenersList = new ArrayList<>();
     }
 
-    @SuppressWarnings("unchecked")
-    private void processQueue() {
-        while (true) {
-            Reference<? extends V> o = queue.poll();
-            if (o == null) {
-                return;
-            }
-            SoftValue<V> k = (SoftValue<V>) o;
-            Object key = k.key;
-            elementCache.remove(key);
-        }
-    }
-
     @Override
     public void put(K key, V value) {
-        processQueue();
-        elementCache.put(key, new SoftValue<V>(value, queue, key));
+        elementCache.put( new SoftReference(key), new SoftReference (value));
         listenersList.forEach(i-> i.notify(key,value,"Put"));
 
     }
@@ -47,21 +28,18 @@ public class MyCache<K, V> implements HwCache<K, V> {
     @Override
     public void remove(K key) {
         listenersList.forEach(i->i.notify(key, get(key),"Remove") );
-        processQueue();
         elementCache.remove(key);
 
     }
 
     @Override
     public V get(Object key) {
-        processQueue();
         SoftReference<V> o = elementCache.get(key);
         if (o == null) {
             return null;
         }
         return o.get();
     }
-
 
     @Override
     public void addListener(HwListener listener) {
@@ -73,15 +51,5 @@ public class MyCache<K, V> implements HwCache<K, V> {
          listenersList.remove(listener);
     }
 
-
-    private static class SoftValue<T> extends SoftReference<T> {
-        final Object key;
-
-        public SoftValue(T ref, ReferenceQueue<T> q, Object key) {
-            super(ref, q);
-            this.key = key;
-        }
-
-    }
 }
 
