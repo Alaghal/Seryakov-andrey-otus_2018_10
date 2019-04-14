7b1dc2534385e030912f0dd5fa58d7239f4c64ad
Andrey <757grok@mail.ru>
2019-04-14 13:08:24 +0700

    Добавил в L16 файлы из примера к лекции

---
+++ L16-SpringBootWS/src/main/java/ru/otus/L16/controllers/MessageController.java
@@ -0,0 +1,23 @@
+package ru.otus.L16.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+import ru.otus.L16.domain.Message;
+
+
+@Controller
+public class MessageController {
+    private static final Logger logger = LoggerFactory.getLogger(MessageController.class);
+
+    @MessageMapping("/message")
+    @SendTo("/topic/response")
+    public Message getMessage(Message message) {
+        logger.info("got message:{}" + message);
+        return new Message(HtmlUtils.htmlEscape(message.getMessageStr()));
+    }
+
+}
